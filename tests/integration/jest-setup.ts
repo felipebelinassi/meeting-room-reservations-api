232cd63cea3c4031e9d23eadaaa779ca7725557c
Felipe Belinassi <felipebelinassi@gmail.com>
2021-01-05 03:47:33 -0300

    Fix jest setup and adjust readme

--- tests/integration/jest-setup.ts
+++ tests/integration/jest-setup.ts
@@ -1,4 +1,3 @@
-import { Server } from 'http';
 import supertest from 'supertest';
 import { app } from '../../src/app';
 import db from '../../src/database';
@@ -6,18 +5,15 @@ import createModels from '../../src/database/models';
 
 jest.mock('../../src/logger');
 
-let server: Server;
 beforeAll(async () => {
   // Instantiate models to sync them correctly
   createModels(db.sequelize);
   await db.sequelize.sync();
 
-  server = app.listen(4000);
-  global.testRequest = supertest(server);
+  global.testRequest = supertest(app);
 });
 
 afterAll(async () => {
   await db.sequelize.drop();
   await db.sequelize.close();
-  server.close();
 });
